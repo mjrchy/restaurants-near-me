254814979fa9b8531cd25a2756a10bbf13e910f6
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-01-25 10:36:55 +0700

    Edit data path

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 from UI import UI
 from data import Data
 
 if __name__ == '__main__':
     ui = UI()
-    data = Data('Lineman_Shops.csv')
+    file_path = 'Lineman_Shops.csv'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = Data(file_path)
     ui.mainloop()
